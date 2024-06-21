00d34972b12a31a40cf59f4877ecc5c2e3434c66
Luca <lucaan@ethz.ch>
2024-06-21 12:51:52 +0200

    changed implementation of CSS selectors

--- src/lib.rs
+++ src/lib.rs
@@ -45,38 +45,43 @@ pub mod goodreads_api {
 
             let document = Html::parse_document(&response);
 
-            // TODO: Select individual books via table tag <tr>, then title and possible series from <a class="bookTitle">
-            // and authors from <a class="authorName">, prevents issues with selecting simpy by <a> etc.
+            let book_selector = Selector::parse(r#"tr[itemscope][itemtype="http://schema.org/Book"]"#).unwrap();
 
-            let tr_selector = Selector::parse(r#"tr[itemscope][itemtype="http://schema.org/Book"]"#).unwrap();
+            let title_series_selector = Selector::parse("a[class=bookTitle]").unwrap();
 
-            let a_selector_title = Selector::parse("a:not([class])").unwrap();
-
-            let a_selector_authors = Selector::parse(r#"a.authorName"#).unwrap();
-
-            let span_selector = Selector::parse(r#"span[itemprop="name"]"#).unwrap();
+            let authors_selector = Selector::parse("a[class=authorName]").unwrap();
 
             let mut books = vec![];
 
-            for tr_element in document.select(&tr_selector) {
+            for book_element in document.select(&book_selector) {
+
+                let title_series_element = book_element.select(&title_series_selector).next().expect("No title found");
                 
-                for (a_element_title, a_element_authors) in izip!(tr_element.select(&a_selector_title), tr_element.select(&a_selector_authors)) {
-                    let title = a_element_title.value().attr("title").expect("No title found").to_string();
-                    println!("Found title: {title}");
-                    let url = "https://www.goodreads.com".to_string() + a_element_title.value().attr("href").expect("No url found");
-                    let mut authors: Vec<String> = Vec::new();
-                    for span_element in a_element_authors.select(&span_selector) {
-                        // Access and print the text content of the <span> element
-                        let author = span_element.text().collect::<Vec<_>>().concat();
-                        println!("Found author name: {}", author);
-                        authors.push(author);
-                    }
-                    // let (authors, pages, series) = extract_metadata_from_book_url(&url);
-                    let pages = 0;
-                    let series = Option::None;
+                // TODO: Change to account for multiple authors
+                let authors_element = book_element.select(&authors_selector).next().expect("No authors found");
+
+                let title_and_series = title_series_element.text().collect::<Vec<_>>().concat();
+                let authors = authors_element.text().map(|x| x.to_string()).collect::<Vec<_>>();
+
+                println!("Found title and series: {}, by {}", title_and_series, authors.concat());
+                
+                // for (a_element_title, a_element_authors) in izip!(tr_element.select(&a_selector_title), tr_element.select(&a_selector_authors)) {
+                //     let title = a_element_title.value().attr("title").expect("No title found").to_string();
+                //     println!("Found title: {title}");
+                //     let url = "https://www.goodreads.com".to_string() + a_element_title.value().attr("href").expect("No url found");
+                //     let mut authors: Vec<String> = Vec::new();
+                //     for span_element in a_element_authors.select(&span_selector) {
+                //         // Access and print the text content of the <span> element
+                //         let author = span_element.text().collect::<Vec<_>>().concat();
+                //         println!("Found author name: {}", author);
+                //         authors.push(author);
+                //     }
+                //     // let (authors, pages, series) = extract_metadata_from_book_url(&url);
+                //     let pages = 0;
+                //     let series = Option::None;
                     
-                    books.push(GoodreadsBook::new(title, authors, pages, series, url));
-                }
+                //     books.push(GoodreadsBook::new(title, authors, pages, series, url));
+                // }
             }
             books
         }
@@ -87,18 +92,17 @@ pub mod goodreads_api {
 }
 
 mod tests {
-    use goodreads_api::GoodreadsBook;
-    use super::*;
+    use super::goodreads_api;
 
     #[test]
     fn test_hobbit() {
         let books = goodreads_api::GoodreadsBook::search("The Hobbit");
-        assert_eq!(books[0..1], vec![GoodreadsBook::new("The Hobbit".to_string(), vec!["J.R.R. Tolkien".to_string()], 310, Option::None, "https://www.goodreads.com/book/show/5907.The_Hobbit?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=1".to_string())]);
+        assert_eq!(books[0..1], vec![goodreads_api::GoodreadsBook::new("The Hobbit".to_string(), vec!["J.R.R. Tolkien".to_string()], 310, Option::None, "https://www.goodreads.com/book/show/5907.The_Hobbit?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=1".to_string())]);
     }
 
     #[test]
     fn test_neverwhere() {
         let books = goodreads_api::GoodreadsBook::search("Neverwhere");
-        assert_eq!(books[0..1], vec![GoodreadsBook::new("Neverwhere".to_string(), vec!["Neil Gaiman".to_string()], 370, Option::None, "https://www.goodreads.com/book/show/14497.Neverwhere?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=2".to_string())]);
+        assert_eq!(books[0..1], vec![goodreads_api::GoodreadsBook::new("Neverwhere".to_string(), vec!["Neil Gaiman".to_string()], 370, Option::None, "https://www.goodreads.com/book/show/14497.Neverwhere?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=2".to_string())]);
     }
 }
