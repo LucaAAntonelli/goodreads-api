de6ce40249753ff4c3b4a8db4bd4dc1f6e5d0ae0
Luca <lucaan@ethz.ch>
2024-06-23 23:14:09 +0200

    scraper now finds multiple authors and series with index

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod goodreads_api {
     // TODO: Change implementation of metadata loading
     // Possible options: threading, only load metadata of chosen book, ...
     use reqwest;
-    use itertools::izip;
+    use regex::Regex;
 
     use scraper::{Html, Selector};
 
@@ -13,26 +13,38 @@ pub mod goodreads_api {
         authors: Vec<String>,
         pages: u64,
         series: Option<String>,
+        index: Option<f32>,
         url: String
     }
 
-    fn extract_metadata_from_book_url(url: &str) -> (Vec<String>, u64, Option<String>) {
+    fn extract_pages_from_url(url: &str) -> u64 {
         let response = reqwest::blocking::get(url).unwrap().text().unwrap();
         let document = Html::parse_document(&response);
 
-        let authors = vec![];
         let pages = 0;
-        let series = Option::None;
 
-        (authors, pages, series)
+        pages
 
         
     }
 
+    fn split(title_and_series: &str) -> Result<(String, Option<String>, Option<f32>), &'static str> {
+        let re = Regex::new(r"^(.*)\s\((.*),\s#(\d+)\)$").unwrap();
+
+        if let Some(captures) = re.captures(title_and_series) {
+            let title = captures.get(1).unwrap().as_str().to_string();
+            let series = captures.get(2).unwrap().as_str().to_string();
+            let index = captures.get(3).unwrap().as_str().parse::<f32>().unwrap();
+            Ok((title, Some(series), Some(index)))
+        } else {
+            Ok((title_and_series.to_string(), None, None))
+        }
+    }
+
     impl GoodreadsBook {
 
-        pub fn new(title: String, authors: Vec<String>, pages: u64, series: Option<String>, url: String) -> Self {
-            Self {title, authors, pages, series, url}
+        pub fn new(title: String, authors: Vec<String>, pages: u64, series: Option<String>, index: Option<f32>, url: String) -> Self {
+            Self {title, authors, pages, series, index, url}
         }
 
         
@@ -57,37 +69,24 @@ pub mod goodreads_api {
 
                 let title_series_element = book_element.select(&title_series_selector).next().expect("No title found");
                 
-                // TODO: Change to account for multiple authors
-                let authors_element = book_element.select(&authors_selector).next().expect("No authors found");
-
-                let title_and_series = title_series_element.text().collect::<Vec<_>>().concat();
-                let authors = authors_element.text().map(|x| x.to_string()).collect::<Vec<_>>();
-
-                println!("Found title and series: {}, by {}", title_and_series, authors.concat());
                 
-                // for (a_element_title, a_element_authors) in izip!(tr_element.select(&a_selector_title), tr_element.select(&a_selector_authors)) {
-                //     let title = a_element_title.value().attr("title").expect("No title found").to_string();
-                //     println!("Found title: {title}");
-                //     let url = "https://www.goodreads.com".to_string() + a_element_title.value().attr("href").expect("No url found");
-                //     let mut authors: Vec<String> = Vec::new();
-                //     for span_element in a_element_authors.select(&span_selector) {
-                //         // Access and print the text content of the <span> element
-                //         let author = span_element.text().collect::<Vec<_>>().concat();
-                //         println!("Found author name: {}", author);
-                //         authors.push(author);
-                //     }
-                //     // let (authors, pages, series) = extract_metadata_from_book_url(&url);
-                //     let pages = 0;
-                //     let series = Option::None;
-                    
-                //     books.push(GoodreadsBook::new(title, authors, pages, series, url));
-                // }
+                let authors_elements = book_element.select(&authors_selector).collect::<Vec<_>>();
+                
+
+                let title_and_series = title_series_element.text().collect::<Vec<_>>().concat().trim().to_string();
+                let url = format!("https://www.goodreads.com{}", title_series_element.value().attr("href").expect("No URL found"));
+
+                let (title, series, index) = split(&title_and_series).unwrap();
+
+                let authors = authors_elements.iter().map(|x| x.text().collect::<Vec<_>>().concat()).collect::<Vec<_>>();
+
+                let pages = extract_pages_from_url(&url);
+                
+                books.push(Self::new(title, authors, pages, series, index, url));
             }
             books
         }
     }
-
-       
 
 }
 
@@ -97,12 +96,12 @@ mod tests {
     #[test]
     fn test_hobbit() {
         let books = goodreads_api::GoodreadsBook::search("The Hobbit");
-        assert_eq!(books[0..1], vec![goodreads_api::GoodreadsBook::new("The Hobbit".to_string(), vec!["J.R.R. Tolkien".to_string()], 310, Option::None, "https://www.goodreads.com/book/show/5907.The_Hobbit?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=1".to_string())]);
+        assert_eq!(books[0..1], vec![goodreads_api::GoodreadsBook::new("The Hobbit".to_string(), vec!["J.R.R. Tolkien".to_string()], 310, Option::None, Option::None, "https://www.goodreads.com/book/show/5907.The_Hobbit?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=1".to_string())]);
     }
 
     #[test]
     fn test_neverwhere() {
         let books = goodreads_api::GoodreadsBook::search("Neverwhere");
-        assert_eq!(books[0..1], vec![goodreads_api::GoodreadsBook::new("Neverwhere".to_string(), vec!["Neil Gaiman".to_string()], 370, Option::None, "https://www.goodreads.com/book/show/14497.Neverwhere?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=2".to_string())]);
+        assert_eq!(books[0..1], vec![goodreads_api::GoodreadsBook::new("Neverwhere".to_string(), vec!["Neil Gaiman".to_string()], 370, Option::None, Option::None, "https://www.goodreads.com/book/show/14497.Neverwhere?from_search=true&from_srp=true&qid=NAtwtTrIMc&rank=2".to_string())]);
     }
 }
